05cfbd0c808f85224ffa4ae92675aca0c9381459
artsiom <artsiom.kachatkou@innowise.com>
2025-06-05 14:34:42 +0200

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Dashboard from "./components/Dashboard";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dashboard Demo</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
